92787b07cd2227ec943ccbed5fc7ea4826ced153
ElkinPrada-TicSocial <elkinprada@ticsocial.com.co>
2025-02-09 17:32:39 -0500

    added app

---
+++ app/main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+from app.routes import predict, health
+
+app = FastAPI(title="ML FastAPI Service")
+
+app.include_router(predict.router)
+app.include_router(health.router)
+
+@app.get("/")
+async def root():
+    return {"message": "API de IA en FastAPI está funcionando"}

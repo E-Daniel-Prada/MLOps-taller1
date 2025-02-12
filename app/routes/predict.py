94b0dab3d29d22fc85a014fe07705383813f9a57
Jorge Reinaldo Linares Pineda <jorgereinaldolinarespineda@MacBook-Pro-de-Jorge.local>
2025-02-11 21:25:39 -0500

    ajuste seleccion del modelo

--- app/routes/predict.py
+++ app/routes/predict.py
@@ -1,14 +1,26 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Body, HTTPException
 import pickle
 import numpy as np
-from fastapi import Body
 
 router = APIRouter(prefix="/predict", tags=["Prediction"])
 
-with open("app/models/LogisticRegression_optimized.pkl", "rb") as f:
-    model = pickle.load(f)
+# Diccionario que mapea IDs a nombres de archivos de modelos
+model_files = {
+    1: "app/models/LogisticRegression_optimized.pkl",
+    2: "app/models/DecisionTree_optimized.pkl",
+    3: "app/models/RandomForest_optimized.pkl"
+}
 
-@router.post("/")
-async def predict(features: list = Body(...)):
+# Función para cargar el modelo según el ID
+def load_model(model_id: int):
+    model_path = model_files.get(model_id)
+    if not model_path:
+        raise HTTPException(status_code=404, detail=f"Modelo con id {model_id} no encontrado.")
+    with open(model_path, "rb") as f:
+        return pickle.load(f)
+
+@router.post("/{id}")
+async def predict(id: int, features: list = Body(...)):
+    model = load_model(id)
     prediction = model.predict(np.array(features).reshape(1, -1))
-    return {"prediction": prediction.tolist()}
+    return {"model_id": model_files.get(id), "prediction": prediction.tolist()}

2ddc00189c29122aff4dc6da6719a09fffc10054
ElkinPrada-TicSocial <elkinprada@ticsocial.com.co>
2025-02-12 20:34:40 -0500

    added api logs in storage

--- app/routes/predict.py
+++ app/routes/predict.py
@@ -2,8 +2,11 @@ from fastapi import APIRouter
 import pickle
 import numpy as np
 from fastapi import Body
+import logging
 
 router = APIRouter(prefix="/predict", tags=["Prediction"])
+
+logging.basicConfig(filename='storage/app.log', level=logging.DEBUG)
 
 with open("app/models/LogisticRegression_optimized.pkl", "rb") as f:
     model = pickle.load(f)
@@ -11,4 +14,5 @@ with open("app/models/LogisticRegression_optimized.pkl", "rb") as f:
 @router.post("/")
 async def predict(features: list = Body(...)):
     prediction = model.predict(np.array(features).reshape(1, -1))
+    logging.info(f"log-prediction: {prediction.tolist()}")
     return {"prediction": prediction.tolist()}
